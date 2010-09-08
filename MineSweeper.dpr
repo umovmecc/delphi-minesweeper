f6006343d8102f5ec73ed3f54e132c4157db1a6f
Daniel Wildt <dwildt@gmail.com>
2010-09-08 01:32:27 -0300

    dwildt: adding initial project files

---
+++ MineSweeper.dpr
@@ -0,0 +1,13 @@
+program MineSweeper;
+
+uses
+  Forms,
+  UFrmMineSweeper in 'src\UFrmMineSweeper.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
